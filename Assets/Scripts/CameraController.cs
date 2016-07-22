356082b11560db199de91802f2ca3e6e6a1dcf25
saikiapriyam <orchotech@gmail.com>
2016-07-22 18:32:10 +0530

    feat : camera moving with character, scene is optimised wrt camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow2dPlatformer : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    Vector3 offset;
+    float lowY;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - target.position;
+        lowY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+        if (target.position.y < lowY)
+        {
+            transform.position = new Vector3(transform.position.x, lowY, transform.position.y);
+        }
+    }
+}
