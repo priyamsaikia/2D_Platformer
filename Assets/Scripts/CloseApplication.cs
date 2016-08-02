f751f54518cd52225713d99daf835df122bd78d7
saikiapriyam <orchotech@gmail.com>
2016-08-02 16:53:35 +0530

    feat: added quit button:

---
+++ Assets/Scripts/CloseApplication.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseApplication : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
